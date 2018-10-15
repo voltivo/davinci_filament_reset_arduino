f16d9c01d5d8694379cc9b137455db1cb9b1f5cb
Jacob Christ <jacob@pontech.com>
2018-10-14 21:20:33 -0700

    Added pinout for the cartridge

--- xyz_dv_eprom.ino
+++ xyz_dv_eprom.ino
@@ -23,6 +23,17 @@ LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 
+/************************************************************
+
+Pinout looking at the pads on the EEPROM board
+
+-------------------\
+|                   \
+|  GND   SCIO   +5V  \
+|                    | 
+----------------------
+ 
+*************************************************************/
 #ifndef _NANODEUNIO_LIB_H
 #define _NANODEUNIO_LIB_H
 
